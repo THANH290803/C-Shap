278077b97f425493db65ca69cb21afea5a7f571d
hoangthanh <phucthanh290803@gmail.com>
2023-05-27 15:14:19 +0700

    CRUD

--- CRUDmysql/CRUDmysql/From2.Designer.cs
+++ CRUDmysql/CRUDmysql/From2.Designer.cs
@@ -44,9 +44,26 @@ namespace CRUDmysql
             this.label1 = new System.Windows.Forms.Label();
             this.dgvCat = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tbPrdId = new System.Windows.Forms.TextBox();
+            this.btnDelPrd = new System.Windows.Forms.Button();
+            this.btnEditPrd = new System.Windows.Forms.Button();
+            this.btnSavePrd = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbMoTa = new System.Windows.Forms.TextBox();
+            this.tbprice = new System.Windows.Forms.TextBox();
+            this.tbNamePrd = new System.Windows.Forms.TextBox();
+            this.lbMota = new System.Windows.Forms.Label();
+            this.lbDanhmuc = new System.Windows.Forms.Label();
+            this.lbpricePrd = new System.Windows.Forms.Label();
+            this.lbNamePrd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvPrd = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCat)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrd)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -157,12 +174,12 @@ namespace CRUDmysql
             // 
             // lbCat
             // 
-            this.lbCat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lbCat.BackColor = System.Drawing.Color.Transparent;
             this.lbCat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lbCat.Enabled = false;
             this.lbCat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lbCat.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbCat.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbCat.Font = new System.Drawing.Font("Times New Roman", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.lbCat.Location = new System.Drawing.Point(779, 225);
             this.lbCat.Name = "lbCat";
             this.lbCat.Size = new System.Drawing.Size(145, 33);
@@ -195,13 +212,199 @@ namespace CRUDmysql
             // 
             // tabPage2
             // 
+            this.tabPage2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage2.BackgroundImage")));
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.tbPrdId);
+            this.tabPage2.Controls.Add(this.btnDelPrd);
+            this.tabPage2.Controls.Add(this.btnEditPrd);
+            this.tabPage2.Controls.Add(this.btnSavePrd);
+            this.tabPage2.Controls.Add(this.btnAdd);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.tbMoTa);
+            this.tabPage2.Controls.Add(this.tbprice);
+            this.tabPage2.Controls.Add(this.tbNamePrd);
+            this.tabPage2.Controls.Add(this.lbMota);
+            this.tabPage2.Controls.Add(this.lbDanhmuc);
+            this.tabPage2.Controls.Add(this.lbpricePrd);
+            this.tabPage2.Controls.Add(this.lbNamePrd);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.dgvPrd);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1247, 735);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Sản Phẩm";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tbPrdId
+            // 
+            this.tbPrdId.Location = new System.Drawing.Point(234, 78);
+            this.tbPrdId.Name = "tbPrdId";
+            this.tbPrdId.Size = new System.Drawing.Size(87, 22);
+            this.tbPrdId.TabIndex = 15;
+            this.tbPrdId.Visible = false;
+            // 
+            // btnDelPrd
+            // 
+            this.btnDelPrd.BackColor = System.Drawing.Color.Red;
+            this.btnDelPrd.Enabled = false;
+            this.btnDelPrd.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnDelPrd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelPrd.Location = new System.Drawing.Point(987, 293);
+            this.btnDelPrd.Name = "btnDelPrd";
+            this.btnDelPrd.Size = new System.Drawing.Size(180, 48);
+            this.btnDelPrd.TabIndex = 14;
+            this.btnDelPrd.Text = "Delete";
+            this.btnDelPrd.UseVisualStyleBackColor = false;
+            this.btnDelPrd.Click += new System.EventHandler(this.btnDelPrd_Click);
+            // 
+            // btnEditPrd
+            // 
+            this.btnEditPrd.BackColor = System.Drawing.Color.Yellow;
+            this.btnEditPrd.Enabled = false;
+            this.btnEditPrd.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.btnEditPrd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditPrd.Location = new System.Drawing.Point(683, 293);
+            this.btnEditPrd.Name = "btnEditPrd";
+            this.btnEditPrd.Size = new System.Drawing.Size(180, 48);
+            this.btnEditPrd.TabIndex = 13;
+            this.btnEditPrd.Text = "Edit";
+            this.btnEditPrd.UseVisualStyleBackColor = false;
+            this.btnEditPrd.Click += new System.EventHandler(this.btnEditPrd_Click);
+            // 
+            // btnSavePrd
+            // 
+            this.btnSavePrd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnSavePrd.Enabled = false;
+            this.btnSavePrd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSavePrd.Location = new System.Drawing.Point(376, 293);
+            this.btnSavePrd.Name = "btnSavePrd";
+            this.btnSavePrd.Size = new System.Drawing.Size(180, 48);
+            this.btnSavePrd.TabIndex = 12;
+            this.btnSavePrd.Text = "Save";
+            this.btnSavePrd.UseVisualStyleBackColor = false;
+            this.btnSavePrd.Click += new System.EventHandler(this.btnSavePrd_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Location = new System.Drawing.Point(72, 293);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(180, 48);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(866, 188);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(254, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // tbMoTa
+            // 
+            this.tbMoTa.Enabled = false;
+            this.tbMoTa.Location = new System.Drawing.Point(866, 132);
+            this.tbMoTa.Name = "tbMoTa";
+            this.tbMoTa.Size = new System.Drawing.Size(254, 22);
+            this.tbMoTa.TabIndex = 9;
+            // 
+            // tbprice
+            // 
+            this.tbprice.Enabled = false;
+            this.tbprice.Location = new System.Drawing.Point(232, 191);
+            this.tbprice.Name = "tbprice";
+            this.tbprice.Size = new System.Drawing.Size(254, 22);
+            this.tbprice.TabIndex = 7;
+            // 
+            // tbNamePrd
+            // 
+            this.tbNamePrd.Enabled = false;
+            this.tbNamePrd.Location = new System.Drawing.Point(232, 138);
+            this.tbNamePrd.Name = "tbNamePrd";
+            this.tbNamePrd.Size = new System.Drawing.Size(254, 22);
+            this.tbNamePrd.TabIndex = 6;
+            // 
+            // lbMota
+            // 
+            this.lbMota.Enabled = false;
+            this.lbMota.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMota.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lbMota.Location = new System.Drawing.Point(757, 132);
+            this.lbMota.Name = "lbMota";
+            this.lbMota.Size = new System.Drawing.Size(129, 22);
+            this.lbMota.TabIndex = 5;
+            this.lbMota.Text = "Mô tả";
+            this.lbMota.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbDanhmuc
+            // 
+            this.lbDanhmuc.Enabled = false;
+            this.lbDanhmuc.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDanhmuc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lbDanhmuc.Location = new System.Drawing.Point(757, 188);
+            this.lbDanhmuc.Name = "lbDanhmuc";
+            this.lbDanhmuc.Size = new System.Drawing.Size(157, 25);
+            this.lbDanhmuc.TabIndex = 4;
+            this.lbDanhmuc.Text = "Danh mục";
+            this.lbDanhmuc.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbpricePrd
+            // 
+            this.lbpricePrd.Enabled = false;
+            this.lbpricePrd.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpricePrd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lbpricePrd.Location = new System.Drawing.Point(96, 193);
+            this.lbpricePrd.Name = "lbpricePrd";
+            this.lbpricePrd.Size = new System.Drawing.Size(107, 18);
+            this.lbpricePrd.TabIndex = 3;
+            this.lbpricePrd.Text = "Giá sản phẩm";
+            this.lbpricePrd.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbNamePrd
+            // 
+            this.lbNamePrd.Enabled = false;
+            this.lbNamePrd.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNamePrd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lbNamePrd.Location = new System.Drawing.Point(96, 138);
+            this.lbNamePrd.Name = "lbNamePrd";
+            this.lbNamePrd.Size = new System.Drawing.Size(148, 22);
+            this.lbNamePrd.TabIndex = 2;
+            this.lbNamePrd.Text = "Tên sản phẩm";
+            this.lbNamePrd.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkOrange;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1251, 60);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sản Phẩm";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgvPrd
+            // 
+            this.dgvPrd.BackgroundColor = System.Drawing.Color.Aqua;
+            this.dgvPrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvPrd.GridColor = System.Drawing.Color.Cyan;
+            this.dgvPrd.Location = new System.Drawing.Point(3, 419);
+            this.dgvPrd.Name = "dgvPrd";
+            this.dgvPrd.ReadOnly = true;
+            this.dgvPrd.RowTemplate.Height = 24;
+            this.dgvPrd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPrd.Size = new System.Drawing.Size(1241, 313);
+            this.dgvPrd.TabIndex = 0;
+            this.dgvPrd.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPrd_CellClick);
             // 
             // From2
             // 
@@ -216,8 +419,32 @@ namespace CRUDmysql
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCat)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrd)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.TextBox tbPrdId;
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSavePrd;
+        private System.Windows.Forms.Button btnEditPrd;
+        private System.Windows.Forms.Button btnDelPrd;
+
+        private System.Windows.Forms.Label lbpricePrd;
+        private System.Windows.Forms.Label lbDanhmuc;
+        private System.Windows.Forms.Label lbMota;
+        private System.Windows.Forms.TextBox tbNamePrd;
+        private System.Windows.Forms.TextBox tbprice;
+        private System.Windows.Forms.TextBox tbMoTa;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Label lbNamePrd;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.DataGridView dgvPrd;
 
         private System.Windows.Forms.TextBox tbId;
 
--- CRUDmysql/CRUDmysql/From2.cs
+++ CRUDmysql/CRUDmysql/From2.cs
@@ -27,6 +27,13 @@ namespace CRUDmysql
             DataSet item = new DataSet();
             adapter.Fill(item, "category");
             dgvCat.DataSource = item.Tables["category"].DefaultView;
+            // PRODUCT VS CATEGORY
+            string sqlProduct = "SELECT p.prd_id, p.prd_name, p.prd_price, p.prd_details, c.cat_name FROM product p INNER JOIN category c ON p.cat_id = c.cat_id ORDER BY p.prd_id ASC";
+            MySqlDataAdapter query = new MySqlDataAdapter(sqlProduct, connection);
+            DataTable itemPrd = new DataTable();
+            query.Fill(itemPrd);
+            dgvPrd.DataSource = itemPrd;
+
         }
 
         private void btnAddnew_Click(object sender, EventArgs e)
@@ -110,6 +117,149 @@ namespace CRUDmysql
 
             tbId.Text = "";
             tbCat.Text = "";
+        }
+
+        private void getCategorise()
+        {
+            MySqlConnection connection = new MySqlConnection(connect);
+            string sql = "SELECT * FROM category";
+            MySqlDataAdapter query = new MySqlDataAdapter(sql, connection);
+            DataTable item = new DataTable();
+            query.Fill(item);
+            comboBox1.DataSource = item;
+
+            comboBox1.DisplayMember = "cat_name";
+
+            comboBox1.ValueMember = "cat_id";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lbNamePrd.Enabled = true;
+            lbpricePrd.Enabled = true;
+            lbMota.Enabled = true;
+            lbDanhmuc.Enabled = true;
+
+            tbNamePrd.Enabled = true;
+            tbprice.Enabled = true;
+            tbMoTa.Enabled = true;
+            comboBox1.Enabled = true;
+            
+            btnSavePrd.Enabled = true;
+            btnEditPrd.Enabled = false;
+            btnDelPrd.Enabled = false;
+            // dgvPrd.CellClick -= dgvPrd_CellClick;
+            // dgvPrd.Enabled = false;
+            getCategorise();
+            
+            tbNamePrd.Text = "";
+            tbprice.Text = "";
+            tbMoTa.Text = "";
+            tbPrdId.Text = "";
+            comboBox1.ResetText();
+
+        }
+
+        private void btnSavePrd_Click(object sender, EventArgs e)
+        {
+            string name = tbNamePrd.Text;
+            string price = tbprice.Text;
+            string mota = tbMoTa.Text;
+            string category = comboBox1.SelectedValue.ToString();
+
+            MySqlConnection connection = new MySqlConnection(connect);
+            string sql = "INSERT INTO product" +
+                         "(prd_name, prd_price, prd_details, cat_id)" +
+                         "VALUES " +
+                         "('"+name+"', "+price+", '"+mota+"', "+category+")";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            From2_Load_1(null,null);
+        }
+
+        private void dgvPrd_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lbNamePrd.Enabled = true;
+            lbpricePrd.Enabled = true;
+            lbMota.Enabled = true;
+            lbDanhmuc.Enabled = true;
+
+            tbNamePrd.Enabled = true;
+            tbprice.Enabled = true;
+            tbMoTa.Enabled = true;
+            comboBox1.Enabled = true;
+            
+            btnSavePrd.Enabled = false;
+            btnEditPrd.Enabled = true;
+            btnDelPrd.Enabled = true;
+            
+            
+            string catName = dgvPrd.Rows[e.RowIndex].Cells["cat_name"].Value.ToString();
+            string id = dgvPrd.Rows[e.RowIndex].Cells["prd_id"].Value.ToString();
+            string prdName = dgvPrd.Rows[e.RowIndex].Cells["prd_name"].Value.ToString();
+            string prdPrice = dgvPrd.Rows[e.RowIndex].Cells["prd_price"].Value.ToString();
+            string prdDetail = dgvPrd.Rows[e.RowIndex].Cells["prd_details"].Value.ToString();
+
+            tbNamePrd.Text = prdName;
+            tbprice.Text = prdPrice;
+            tbMoTa.Text = prdDetail;
+            tbPrdId.Text = id;
+            getCategorise();
+            int index = comboBox1.FindStringExact(catName);
+            if (index != -1)
+            {
+                comboBox1.SelectedIndex = index;
+            }
+        }
+
+        private void btnEditPrd_Click(object sender, EventArgs e)
+        {
+            string namePrd = tbNamePrd.Text;
+            string price = tbprice.Text;
+            string mota = tbMoTa.Text;
+            int id = int.Parse(tbPrdId.Text);
+            string category = comboBox1.SelectedValue.ToString();
+
+            MySqlConnection connection = new MySqlConnection(connect);
+            
+            string sql = "UPDATE product SET " +
+                         "prd_name= '" + namePrd + "', prd_price = '" + price + "', prd_details= '" + mota + "', cat_id= '" + category +
+                         "' WHERE prd_id = '" + id + "'";
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            From2_Load_1(null,null);
+
+            tbNamePrd.Text = "";
+            tbprice.Text = "";
+            tbMoTa.Text = "";
+            tbPrdId.Text = "";
+            comboBox1.ResetText();
+        }
+
+        private void btnDelPrd_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(tbPrdId.Text);
+
+            MySqlConnection connection = new MySqlConnection(connect);
+            
+            string sql = "DELETE FROM product WHERE prd_id='"+ id +"'";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            From2_Load_1(null, null);
+
+            tbNamePrd.Text = "";
+            tbprice.Text = "";
+            tbMoTa.Text = "";
+            tbPrdId.Text = "";
+            comboBox1.ResetText();
         }
     }
 }
